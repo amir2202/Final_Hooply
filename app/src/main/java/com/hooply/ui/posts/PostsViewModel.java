3b6682d5b4037c20b64f9868ec408a5e538d8a10
tm2u19 <tm2u19@soton.ac.uk>
2020-06-20 21:01:43 +0200

    NEEDS FIXING OMMG

--- app/src/main/java/com/hooply/ui/posts/PostsViewModel.java
+++ app/src/main/java/com/hooply/ui/posts/PostsViewModel.java
@@ -1,16 +1,16 @@
-//package com.hooply.ui.home;
+//package com.hooply.ui.posts;
 //
 //import androidx.lifecycle.LiveData;
 //import androidx.lifecycle.MutableLiveData;
 //import androidx.lifecycle.ViewModel;
 //
-//public class HomeViewModel extends ViewModel {
+//public class PostsViewModel extends ViewModel {
 //
 //    private MutableLiveData<String> mText;
 //
-//    public HomeViewModel() {
+//    public PostsViewModel() {
 //        mText = new MutableLiveData<>();
-//        mText.setValue("This is home fragment");
+//        mText.setValue("This is posts fragment");
 //    }
 //
 //    public LiveData<String> getText() {
